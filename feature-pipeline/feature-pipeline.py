59836f61e6cef96846ae48398f41dcc66cbf238b
jaeyow <jaeyow@gmail.com>
2023-12-16 13:32:29 +1100

    add poetry dependency manager

--- feature-pipeline/feature-pipeline.py
+++ feature-pipeline/feature-pipeline.py
@@ -1,7 +1,7 @@
 # pylint: disable=C0415,C0103,W0201,W0702,W0718
 import os
 from metaflow import FlowSpec, step
-from comet_ml.integration.metaflow import comet_flow
+# from comet_ml.integration.metaflow import comet_flow
 
 try:
     from dotenv import load_dotenv
@@ -11,7 +11,7 @@ except Exception:
     print("No dotenv package")
 
 
-@comet_flow(project_name="<please-replace-me>")
+# @comet_flow(project_name="<please-replace-me>")
 class F1PredictionFeaturePipeline(FlowSpec):
 
     @step
@@ -21,14 +21,15 @@ class F1PredictionFeaturePipeline(FlowSpec):
         in order like environment variables, connection strings, etc, and if there are
         any issues, quit now.
         """
-        assert os.environ["AWS_PROFILE_NAME"]
-        assert os.environ["AWS_DEFAULT_REGION"]
-        assert os.environ["BUCKET_NAME"]
-        assert os.environ["COMET_API_KEY"]
+        # assert os.environ["AWS_PROFILE_NAME"]
+        # assert os.environ["AWS_DEFAULT_REGION"]
+        # assert os.environ["BUCKET_NAME"]
+        # assert os.environ["COMET_API_KEY"]
 
-        self.AWS_PROFILE_NAME = os.environ["AWS_PROFILE_NAME"]
-        self.AWS_DEFAULT_REGION = os.environ["AWS_DEFAULT_REGION"]
-        self.BUCKET_NAME = os.environ["BUCKET_NAME"]
+        # self.AWS_PROFILE_NAME = os.environ["AWS_PROFILE_NAME"]
+        # self.AWS_DEFAULT_REGION = os.environ["AWS_DEFAULT_REGION"]
+        # self.BUCKET_NAME = os.environ["BUCKET_NAME"]
+        print("F1PredictionFeaturePipeline...")
 
         self.next(self.end)
 
