74c9975ee6ed981534403afd279dd0c8d7ccc021
jaeyow <jaeyow@gmail.com>
2023-12-21 19:38:44 +1100

    add construcor experience

--- feature-pipeline/feature-pipeline.py
+++ feature-pipeline/feature-pipeline.py
@@ -3,12 +3,12 @@ import os
 from metaflow import FlowSpec, step
 # from comet_ml.integration.metaflow import comet_flow
 
-try:
-    from dotenv import load_dotenv
+# try:
+#     from dotenv import load_dotenv
 
-    load_dotenv(verbose=True, dotenv_path=".env")
-except Exception:
-    print("No dotenv package")
+#     load_dotenv(verbose=True, dotenv_path=".env")
+# except Exception:
+#     print("No dotenv package")
 
 
 # @comet_flow(project_name="<please-replace-me>")
@@ -16,22 +16,19 @@ class F1PredictionFeaturePipeline(FlowSpec):
 
     @step
     def start(self):
-        import duckdb
         """
         Initialization, place everything init related here, check that everything is
         in order like environment variables, connection strings, etc, and if there are
         any issues, quit now.
         """
-        # assert os.environ["AWS_PROFILE_NAME"]
-        # assert os.environ["AWS_DEFAULT_REGION"]
-        # assert os.environ["BUCKET_NAME"]
-        # assert os.environ["COMET_API_KEY"]
+        assert os.environ["FS_API_KEY"]
+        assert os.environ["FS_PROJECT_NAME"]
 
-        # self.AWS_PROFILE_NAME = os.environ["AWS_PROFILE_NAME"]
-        # self.AWS_DEFAULT_REGION = os.environ["AWS_DEFAULT_REGION"]
-        # self.BUCKET_NAME = os.environ["BUCKET_NAME"]
+        self.FS_API_KEY = os.environ["FS_API_KEY"]
+        self.FS_PROJECT_NAME = os.environ["FS_PROJECT_NAME"]
+
         print("F1PredictionFeaturePipeline...")
-        
+
         self.next(self.get_dataset)
 
     @step
@@ -72,26 +69,26 @@ class F1PredictionFeaturePipeline(FlowSpec):
                 drivers.driverId,
                 drivers.surname,
                 drivers.forename,
-                standings.points,
+                driver_standings.points,
                 races.name,
                 races.year
-            FROM '../data/drivers.csv' as drivers
-            INNER JOIN '../data/driver_standings.csv' as standings
-                ON drivers.driverId = standings.driverId
-            INNER JOIN '../data/races.csv' as races
-                ON standings.raceId = races.raceId
-            WHERE surname = 'Ricciardo' AND standings.position <= 5
-            ORDER BY standings.points DESC;
+            FROM drivers
+            INNER JOIN driver_standings
+                ON drivers.driverId = driver_standings.driverId
+            INNER JOIN races
+                ON driver_standings.raceId = races.raceId
+            WHERE surname = 'Ricciardo' AND driver_standings.position <= 5
+            ORDER BY driver_standings.points DESC;
             """
         ).df()
         print(drv_df.head(5))
 
-        results_store = con.execute(
+        results_store_df = con.execute(
             """
             SELECT * FROM results_store;
             """
         ).df()
-        print(results_store.head(5))
+        print(results_store_df.head(5))
         
         self.next(self.create_driver_experience_feature)
 
@@ -125,23 +122,9 @@ class F1PredictionFeaturePipeline(FlowSpec):
             """
         )
 
-        con.execute(
-            """
-            SELECT
-                drivers.driverId,
-                drivers.surname,
-                drivers.forename,
-                races.name,
-                races.year,
-                races.date,
-                results_store.driverExperience,
-            FROM results_store
-            INNER JOIN drivers ON results_store.driverId = drivers.driverId
-            INNER JOIN races ON results_store.raceId = races.raceId
-            WHERE surname = 'Leclerc' AND races.year = 2023
-            ORDER BY races.date ASC 
-            """
-        ).df()
+
+        results_store_df = con.execute("SELECT * FROM results_store;").df()
+        self.results_store = results_store_df
 
         print(
         f"""
@@ -154,11 +137,155 @@ class F1PredictionFeaturePipeline(FlowSpec):
             Driver's experience in Formula 1, where a more experienced F1 driver
             typically places better than a rookie.
 
-            Added new feature: 'DriverExperience' to results_store table.
+            Added new feature: 'DriverExperience' to results_store table. {self.results_store.shape}
+            
         """
         )
 
+        self.next(self.create_constructor_experience_feature)
+
+    @step
+    def create_constructor_experience_feature(self):
+        """
+        Feature Engineering: Constructor's experience in Formula 1, where a more experienced F1
+        constructor typically places better than a rookie. 
+        """
+        import duckdb
+        import numpy as np
+        import pandas as pd
+
+        con = duckdb.connect(database = "f1-race-data-2023.duckdb", read_only = False)
+        con.execute(
+            """
+            ALTER TABLE results_store
+            ADD COLUMN IF NOT EXISTS constructorExperience INT DEFAULT 0;
+
+            UPDATE results_store
+            SET constructorExperience = subquery.cumulative_experience
+            FROM (
+                SELECT 
+                    rs.constructorId,
+                    r.raceId,
+                    CASE WHEN ROW_NUMBER() OVER (PARTITION BY rs.constructorId ORDER BY r.date DESC) <= 60 THEN 1 ELSE 0 END AS cumulative_experience
+                FROM results_store AS rs
+                INNER JOIN races AS r ON rs.raceId = r.raceId
+            ) AS subquery
+            WHERE results_store.constructorId = subquery.constructorId AND results_store.raceId = subquery.raceId;
+            """
+        )      
+
+        results_store_df = con.execute("SELECT * FROM results_store;").df()
+        self.results_store = results_store_df
+
+        print(
+        f"""
+        ***************************************************************************
+
+            Feature Engineering - Constructor Experience
+
+        ***************************************************************************
+
+            Constructor's experience in Formula 1, where a more experienced F1
+            constructor typically places better than a rookie.
+
+            Added new feature: 'ConstructorExperience', new dataframe shape: {self.results_store.shape}
+        """
+        )
+
+        self.next(self.finalise_feature_list)  
+
+    @step
+    def finalise_feature_list(self):
+        """
+        Whittle down the feature list to the most important features.
+        """
+        self.results_store.drop(['resultId','number','positionText','positionOrder','time','milliseconds'], axis=1, inplace=True)
+
+        self.next(self.create_feature_group)
+
+    @step
+    def create_feature_group(self):
+        """
+        create_feature_group
+        """
+        import hopsworks
+        import pandas as pd
+        # from great_expectations.core import ExpectationSuite
+        # from hsfs.feature_group import FeatureGroup
+
+        # Connect to feature store.
+        project = hopsworks.login(
+            api_key_value=self.FS_API_KEY, project=self.FS_PROJECT_NAME
+        )
+        feature_store = project.get_feature_store()
+
+        # Create feature group.
+        f1_prediction_fg = feature_store.get_or_create_feature_group(
+            name="f1_prediction_fg_1",
+            primary_key=["driverId"],
+            online_enabled=True,
+            version=3,
+            description="Blah blah blah",
+            time_travel_format=None,
+        )
+        # Upload data.
+        f1_prediction_fg.insert(
+            features=self.results_store,
+            overwrite=False,
+            write_options={
+                "wait_for_job": True,
+            },
+        )
+
+        # Add feature descriptions.
+        # resultId,raceId,driverId,constructorId,number,grid,position,positionText,positionOrder,points,
+        # laps,time,milliseconds,fastestLap,rank,fastestLapTime,fastestLapSpeed,statusId
+        # feature_descriptions = [
+        #     {
+        #         "name": "datetime_utc",
+        #         "description": """
+        #                         Datetime interval in UTC when the data was observed.
+        #                         """,
+        #         "validation_rules": "Always full hours, i.e. minutes are 00",
+        #     },
+        #     {
+        #         "name": "area",
+        #         "description": """
+        #                         Denmark is divided in two price areas, divided by the Great Belt: DK1 and DK2.
+        #                         If price area is “DK”, the data covers all Denmark.
+        #                         """,
+        #         "validation_rules": "0 (DK), 1 (DK1) or 2 (Dk2) (int)",
+        #     },
+        #     {
+        #         "name": "consumer_type",
+        #         "description": """
+        #                         The consumer type is the Industry Code DE35 which is owned by Danish Energy. 
+        #                         The code is used by Danish energy companies.
+        #                         """,
+        #         "validation_rules": ">0 (int)",
+        #     },
+        #     {
+        #         "name": "energy_consumption",
+        #         "description": "Total electricity consumption in kWh.",
+        #         "validation_rules": ">=0 (float)",
+        #     },
+        # ]
+        # for description in feature_descriptions:
+        #     energy_feature_group.update_feature_description(
+        #         description["name"], description["description"]
+        #     )
+
+        # Update statistics.
+        f1_prediction_fg.statistics_config = {
+            "enabled": True,
+            "histograms": True,
+            "correlations": True,
+        }
+        f1_prediction_fg.update_statistics_config()
+        f1_prediction_fg.compute_statistics()
+
         self.next(self.end)
+
 
     @step
     def end(self):
